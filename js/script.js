58eea3d991bcfcbebcd386f58bba16ff207ef743
Morkovka171 <guganowa@yandex.ru>
2022-11-14 23:12:27 +0300

    JS

--- js/script.js
+++ js/script.js
@@ -7,3 +7,27 @@ const swiper = new Swiper('.slider-main-block', {
       prevEl: '.body-main-block__arrow__arrow.swiper-button-prev',
     },
   });
+
+  //Таб
+const TabNavItems = document.querySelectorAll('.tabs-deals__button');
+const TabItems = document.querySelectorAll('.item-tabs');
+document.addEventListener('click', function(e) {
+  const targetElement = e.target;
+  let currentActiveIndex = null;
+  let newActiveIndex = null;
+  if(targetElement.closest('.tabs-deals__button')) {
+    TabNavItems.forEach((TabNavItem, index) => {
+      if(TabNavItem.classList.contains('active')){
+        currentActiveIndex = index;
+        TabNavItem.classList.remove('active');
+      }
+      if(TabNavItem === targetElement){
+        newActiveIndex = index;
+      }
+      //
+    });
+    targetElement.classList.add('active');
+    TabItems[currentActiveIndex].classList.remove('active');
+    TabItems[newActiveIndex].classList.add('active');
+  }
+});
