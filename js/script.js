39557e07ceafd2b0d63224a907c7067280e8d103
Morkovka171 <guganowa@yandex.ru>
2022-11-15 15:13:47 +0300

    Стили блока Featured + Адаптив

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const swiper = new Swiper('.slider-main-block', {
     // Navigation arrows
     navigation: {
       nextEl: '.body-main-block__arrow.swiper-button-next',
-      prevEl: '.body-main-block__arrow__arrow.swiper-button-prev',
+      prevEl: '.body-main-block__arrow.swiper-button-prev',
     },
   });
 
